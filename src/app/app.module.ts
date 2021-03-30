273812283d0c632901be50da509c1a6d7d4d9c60
unknown <malbuquerquedesouza@gmail.com>
2021-03-30 18:54:38 -0300

    criação da pasta home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  //declarando e incluindo os componentes
+    AppComponent, HomeComponent
   ],
-  imports: [
+  imports: [ //inports do componentes esternos. Ex componentes do angular material
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
