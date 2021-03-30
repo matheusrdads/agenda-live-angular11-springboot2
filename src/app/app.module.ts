d7a55ad97d6e64417b4d4c36cb8717e1045ee13b
unknown <malbuquerquedesouza@gmail.com>
2021-03-30 19:27:47 -0300

    inserindo componentes do angula material, toolbar e button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [  //declarando e incluindo os componentes
@@ -13,7 +17,10 @@ import { HomeComponent } from './views/home/home.component';
   imports: [ //inports do componentes esternos. Ex componentes do angular material
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
